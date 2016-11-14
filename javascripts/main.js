6c8412f33eebd4c347cb9350f42a79624e137d43
Sebastian Kapunkt <Kindt.sebastian@outlook.com>
2016-11-14 10:33:07 +0100

    adds styling and systemdiagram

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,15 @@
-console.log('This would be the main JS file.');
+function hideall() {
+    $('#persona').css("display", "none");
+    $('#system').css("display", "none");
+}
+
+$('#persona-link').click(function() {
+    hideall();
+    $('#persona').css("display", "flex");
+});
+
+$('#system-link').click(function() {
+    hideall();
+    $('#system').css("display", "flex");
+});
+
