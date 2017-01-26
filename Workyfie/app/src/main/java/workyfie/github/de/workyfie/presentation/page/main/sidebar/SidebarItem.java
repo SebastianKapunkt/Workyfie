9b52b7217515b4b3338ce415a5f6ef1496bc78ce
ThunderAnimal <MartinWeber.93@outlook.com>
2017-01-26 01:39:41 +0100

    adds title in sidebaritem

--- Workyfie/app/src/main/java/workyfie/github/de/workyfie/presentation/page/main/sidebar/SidebarItem.java
+++ Workyfie/app/src/main/java/workyfie/github/de/workyfie/presentation/page/main/sidebar/SidebarItem.java
@@ -3,16 +3,18 @@ package workyfie.github.de.workyfie.presentation.page.main.sidebar;
 import workyfie.github.de.workyfie.R;
 
 public enum SidebarItem {
-    MEASURE(R.drawable.dna, R.string.sidebar_measure),
-    HISTORY(R.drawable.book_open_varaint, R.string.sidebar_history),
-    SENSOR(R.drawable.bluetooth_transfer, R.string.sidebar_sensor),
-    INFORMATION(R.drawable.information_variant, R.string.information);
+    MEASURE(R.drawable.dna, R.string.sidebar_measure,  R.string.app_name),
+    HISTORY(R.drawable.book_open_varaint, R.string.sidebar_history, R.string.sidebar_history),
+    SENSOR(R.drawable.bluetooth_transfer, R.string.sidebar_sensor, R.string.sidebar_sensor),
+    INFORMATION(R.drawable.information_variant, R.string.information, R.string.information);
 
     public final int resIdIcon;
     public final int resIdLabel;
+    public final int resTitle;
 
-    SidebarItem(int resIdIcon, int resIdLabel) {
+    SidebarItem(int resIdIcon, int resIdLabel, int resTitle) {
         this.resIdIcon = resIdIcon;
         this.resIdLabel = resIdLabel;
+        this.resTitle = resTitle;
     }
 }
