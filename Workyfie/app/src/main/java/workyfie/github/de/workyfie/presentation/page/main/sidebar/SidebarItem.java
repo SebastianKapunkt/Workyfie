3fde09d4144f4c139282cbfdfc2a3a09ba0ccdf3
Martin <MartinWeber.93@outlook.com>
2017-01-27 13:25:53 +0100

    fix sidebar icons (#20)

--- Workyfie/app/src/main/java/workyfie/github/de/workyfie/presentation/page/main/sidebar/SidebarItem.java
+++ Workyfie/app/src/main/java/workyfie/github/de/workyfie/presentation/page/main/sidebar/SidebarItem.java
@@ -5,7 +5,7 @@ import workyfie.github.de.workyfie.R;
 public enum SidebarItem {
     MEASURE(R.drawable.dna, R.string.sidebar_measure,  R.string.app_name),
     HISTORY(R.drawable.book_open_varaint, R.string.sidebar_history, R.string.sidebar_history),
-    SENSOR(R.drawable.bluetooth_transfer, R.string.sidebar_sensor, R.string.sidebar_sensor),
+    SENSOR(R.drawable.bluetooth_setting, R.string.sidebar_sensor, R.string.sidebar_sensor),
     INFORMATION(R.drawable.information_variant, R.string.information, R.string.information);
 
     public final int resIdIcon;
